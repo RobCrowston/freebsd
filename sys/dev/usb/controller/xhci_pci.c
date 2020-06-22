4862cbc0ee9cc7b3a463e8206de503e71c3edb7c
Robert Crowston <crowston@protonmail.com>
2020-06-22 22:13:29 +0100

    Add bcm2838 pci driver.

--- sys/dev/usb/controller/xhci_pci.c
+++ sys/dev/usb/controller/xhci_pci.c
@@ -63,7 +63,6 @@ __FBSDID("$FreeBSD$");
 #include "usb_if.h"
 
 static device_probe_t xhci_pci_probe;
-static device_attach_t xhci_pci_attach;
 static device_detach_t xhci_pci_detach;
 static usb_take_controller_t xhci_pci_take_controller;
 
@@ -85,6 +84,9 @@ static driver_t xhci_driver = {
 	.methods = xhci_device_methods,
 	.size = sizeof(struct xhci_softc),
 };
+
+DEFINE_CLASS_0(xhci, xhci_pci_driver, xhci_device_methods,
+    sizeof(struct xhci_softc));
 
 static devclass_t xhci_devclass;
 
@@ -223,7 +225,7 @@ xhci_pci_port_route(device_t self, uint32_t set, uint32_t clear)
 	return (0);
 }
 
-static int
+int
 xhci_pci_attach(device_t self)
 {
 	struct xhci_softc *sc = device_get_softc(self);
